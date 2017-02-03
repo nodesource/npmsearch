a6f4177d61b25a5a38947db1456e155f718dc778
Kate Kuchin <kate.kuchin@gmail.com>
2017-02-03 11:31:25 -0800

    about page content and styling

--- public/about.js
+++ public/about.js
@@ -1,6 +1,16 @@
 module.exports = renderAbout
 
 function renderAbout () {
+  const criteria = {
+    readme: 'Package has a README file',
+    sourceControl: 'Package\'s source code is in public source control',
+    license: 'Package has an Apache, BSD, ISC, or MIT LICENSE file',
+    usage: 'Disk usage after npm install is < 25 MB',
+    tests: 'Package has passing tests',
+    coverage: 'Package has at least 70% test coverage',
+    vulnerabilities: 'Package has no known security vulnerabilities'
+  }
+
   return `
     <html>
       <head>
@@ -11,7 +21,7 @@ function renderAbout () {
         <link type="text/css" rel="stylesheet" media="all" href="core.css" />
       </head>
       <body>
-        <div class="main-content">
+        <div class="main-content about">
           <header>
             <section id="heading">
               <h1>npm</h1>
@@ -32,9 +42,44 @@ function renderAbout () {
               </div>
             </section>
           </header>
+          <hr />
 
           <section id="main-copy">
-          <hr style="margin:0" />
+            <h1>About</h1>
+            <h2>Nodesource Certification Score</h2>
+            <p>
+              npmsearch uses The NodeSource Certification Process, an extensive suite of
+              tests based on attributes that are valuable to customers. We are attempting
+              to capture the best signals that determine the quality, security and overall
+              health of any given package in the npm ecosystem.
+            </p>
+            <br />
+            <p>
+              We expect the calculation of this score to change over time as we incorporate 
+              feedback about additional signals that are important to professional users of 
+              Node.js.
+            </p>
+            <h2>Scoring Criteria</h2>
+            <p>The current scoring criteria used are:</p>
+            <ul>
+              <li><span>${criteria.readme}</span></li>
+              <li><span>${criteria.sourceControl}</span></li>
+              <li><span>${criteria.license}</span></li>
+              <li><span>${criteria.usage}</span></li>
+              <li><span>${criteria.tests}</span></li>
+              <li><span>${criteria.coverage}</span></li>
+              <li><span>${criteria.vulnerabilities}</span></li>
+            </ul>
+            <h2>Credits</h2>
+            <p>
+              npmsearch is made possible by NodeSource, The Node Company™. NodeSource is the
+              Node Company, offering N|Solid - the most secure platform for running Node.js
+              in production. Trusted by MasterCard, GoPro, Condé Nast and many others, N|Solid
+              secures your applications and provides surgical insight into your Node.js
+              processes to help you identify problems before they happen. With a commitment
+              to the Node.js community, NodeSource is dedicated to helping build an open,
+              stable, long-lasting ecosystem for Node.js.
+            </p>
 
           </section>
 
